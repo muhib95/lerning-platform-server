decde7a9b3801a09699bef49f4f232f736ca6244
Muhibbul Hasan <muhibbulhasan6@gmail.com>
2022-10-25 00:55:03 +0600

    catagories add

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+
+const cors = require('cors')
+const catagories=require('./data/catagory.json')
 const app = express()
 const port = 5000
 
+app.use(cors())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Heuyyllo World!')
+})
+app.get('/catagories',(req,res)=>{
+  res.send(catagories)
 })
 
 app.listen(port, () => {
