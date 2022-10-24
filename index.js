bdec1b96a873e7f8409c66b300d3aac8d38f94dd
Muhibbul Hasan <muhibbulhasan6@gmail.com>
2022-10-25 02:17:20 +0600

    modules data implement

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const cors = require('cors')
 const catagories=require('./data/catagory.json')
+const modules=require('./data/modules.json')
 const app = express()
 const port = 5000
 
@@ -12,6 +13,21 @@ app.get('/', (req, res) => {
 })
 app.get('/catagories',(req,res)=>{
   res.send(catagories)
+})
+app.get('/catagories/:id',(req,res)=>{
+  const id=req.params.id;
+  const module=modules.find(m=>m.cid==id)
+  res.send(module)
+})
+app.get('/modules',(req,res)=>{
+  res.send(modules)
+
+})
+app.get('/modules/:id',(req,res)=>{
+  const id=req.params.id;
+  const module=modules.find(m=>m.id==id)
+  res.send(module)
+
 })
 
 app.listen(port, () => {
